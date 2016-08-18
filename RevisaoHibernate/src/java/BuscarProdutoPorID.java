470e59cdea872ab6e49d7668a6bbb68cfd41b52b
ra21505955 <ra21505955@T1-120409-57459.dinfor.uniceub>
2016-08-18 20:42:25 -0300

    13

---
+++ RevisaoHibernate/src/java/BuscarProdutoPorID.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ *
+ * @author RA21505955
+ */
+public class BuscarProdutoPorID extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet BuscarProdutoPorID</title>");            
+            out.println("</head>");
+            out.println("<body>");
+             Session sessao = HibernateUtil
+                    .getSessionFactory()
+                    .openSession();
+             
+            /*int idForm = 2;
+             Produto p = (Produto) sessao.get(Produto.class, idForm );*/
+          /* if(p != null){
+           
+           }*/
+          
+          Criteria  c = sessao.createCriteria(Produto.class);
+          c.add(Restrictions.ge("preco", 10));
+        
+           List<Produto> produtos = c.list();
+           
+           Transaction t = sessao.beginTransaction();
+           /*p.setNome("Nome batata");*/
+           for(Produto p : produtos){
+           p.setPreco(10);
+           sessao.save(p);
+           }
+          
+           sessao.flush();
+           t.commit();
+           
+           sessao.close();
+                   
+           
+           
+            out.println("<h1>Servlet BuscarProdutoPorID at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ RevisaoHibernate/src/java/Produto.java
@@ -0,0 +1,42 @@
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RA21505955
+ */
+@Entity
+
+public class Produto {
+    private float preco;
+
+    private String nome;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int identificador;
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
